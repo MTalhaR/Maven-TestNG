d54095eccae1f4e001217457c870e4e5b7028eea
MTalhaR <talhariaz021@gmail.com>
2022-11-26 22:03:26 +0500

    class 01

---
+++ src/test/java/class01/testNGAnnotations.java
@@ -0,0 +1,24 @@
+package class01;
+
+import org.testng.annotations.Test;
+
+public class testNGAnnotations {
+    // first test case
+    @Test
+    public void firstTestCase(){
+        System.out.println("i am the first test case");
+    }
+
+    @Test
+    public void secondTestCase(){
+        System.out.println("i am the second test case");
+    }
+
+    @Test
+    public void thirdTestCase(){
+        System.out.println("i am the third test case");
+    }
+
+
+
+}
